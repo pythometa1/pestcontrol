f969353e6593f0684c85519a317cbb4c2447ffed
Shubham <blackhat2712@gmail.com>
2023-09-29 12:48:12 +0530

    first commit

--- client/src/Pages/ServiceBook.jsx
+++ client/src/Pages/ServiceBook.jsx
@@ -183,10 +183,10 @@ const ServiceBook = () => {
                   window.location.href = "https://api.whatsapp.com/send?phone=8010281236";
                 }}
                 className="bg-green-500 ml-[-12px] text-white px-4 py-2 mr-8 rounded-md">
-                
-Whatsapp              </button>
 
-             
+                Whatsapp              </button>
+
+
               {/* <button 
                       target="_blank"
 
